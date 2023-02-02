29338069b416aeb53af5c0c670cb6407dcce2865
rbqueiroz <30379824+rbqueiroz@users.noreply.github.com>
2023-02-02 16:06:25 -0300

    novo

---
+++ src/cursojava/thread/ExecultarTela.java
@@ -0,0 +1,10 @@
+package cursojava.thread;
+
+public class ExecultarTela {
+	
+	public static void main(String[] args) {
+		
+		TelaTimeThread telaTimeThread = new TelaTimeThread();
+	}
+
+}
---
+++ src/cursojava/thread/TelaTimeThread.java
@@ -0,0 +1,19 @@
+package cursojava.thread;
+
+import javax.swing.JDialog;
+
+public class TelaTimeThread extends JDialog{
+
+	public TelaTimeThread() {
+		// TODO Auto-generated constructor stub
+		
+		
+		
+		
+		
+		
+		
+		//sera o ultimo comando
+		setVisible(true); // torna a tela visivel para o usuairo
+	}
+}
